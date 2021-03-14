d2588977b95b97f4b695c80d7160f865efcbd90d
Christian Dueck <christian@dueck.com.br>
2021-03-14 17:49:36 -0300

    Creating menu feature

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import styles from './styles/Menu.module.scss';
 
 export function Menu() {
+    const [collapsed, setCollapsed] = useState(true);
+
+    function toggleCollapsed() {
+        setCollapsed(!collapsed);
+    }
+
     return (
-        <button
-            className={styles.menuButton}
-            type="button"
-        >
-            <div></div>
-            <div></div>
-        </button>
+        <div className={styles.menu}>
+            <button
+                className={styles.menuButton}
+                onClick={toggleCollapsed}
+                type="button"
+            >
+                <div className={collapsed ? '' : styles.collapsed}></div>
+                <div className={collapsed ? '' : styles.collapsed}></div>
+            </button>
+            <div className={`${styles.fullMenu} ${collapsed ? styles.collapsed : ''}`}>
+                <ul>
+                    <li>
+                        <img src="./icons/add_circle.svg" alt="Add icon" />
+                        Adicionar oração
+                    </li>
+                    <li>
+                        <img src="./icons/pray_menu.svg" alt="Pray icon" />
+                        Lista de orações
+                    </li>
+                    <li>
+                        <img src="./icons/settings.svg" alt="Settings icon" />
+                        Configurar notificações
+                    </li>
+                </ul>
+                <div className={styles.logo}>
+
+                </div>
+            </div>
+        </div>
     );
 }
