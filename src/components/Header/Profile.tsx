f1f5089efb872b854cd49a6aa3fde321c46bff63
Christian Dueck <christian@dueck.com.br>
2021-03-14 16:34:43 -0300

    Creation of profile menu

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import styles from './styles/Profile.module.scss';
 
 export function Profile() {
+    const [collapsed, setCollapsed] = useState(true);
+
+    function toggleCollapsed() {
+        setCollapsed(!collapsed);
+    }
+
     return (
         <div className={styles.container}>
-            <h1>Konrad</h1>
-            <img src="./icons/profile.svg" alt="Profile icon" />
+
+            <div
+                className={`${styles.profile} ${collapsed ? styles.collapsed : ''}`}
+                onClick={toggleCollapsed}
+            >
+                <button
+                    className={collapsed ? styles.collapsed : ''}
+                >
+                    <img src="./icons/exit.svg" alt="Exit icon" />
+                </button>
+                <div>
+                    <h1>Konrad</h1>
+                    <img src="./icons/profile.svg" alt="Profile icon" />
+                </div>
+            </div>
+            <div className={`${styles.menu} ${collapsed ? styles.collapsed : ''}`}>
+                <a>Editar dados pessoais</a>
+                <a>Sair</a>
+            </div>
         </div>
     );
 }
